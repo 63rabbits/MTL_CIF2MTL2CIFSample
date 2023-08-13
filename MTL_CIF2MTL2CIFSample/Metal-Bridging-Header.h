9fbcb8e7a7f910446f77c128874c522b8fd1a54c
63rabbits goodman <63rabbitsgoodman@gmail.com>
2023-08-13 19:45:54 +0900

    Updated to use bridge header. Specify midTexture storage mode as private.

---
+++ MTL_CIF2MTL2CIFSample/Metal-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Metal-Bridging-Header.h
+//  MTL_CIF2MTL2CIFSample
+//
+//  Created by 63rabbits goodman on 2023/08/13.
+//
+
+#ifndef Metal_Bridging_Header_h
+#define Metal_Bridging_Header_h
+
+#include "MetalShader.h"
+
+#endif /* Metal_Bridging_Header_h */
